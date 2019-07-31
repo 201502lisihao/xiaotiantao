e0c3383d3a78b20965d96a548cd93f0b4604d1b1
201502lisihao <735407073@qq.com>
2019-07-31 12:42:30 +0800

    update

--- common/config/main.php
+++ common/config/main.php
@@ -3,7 +3,19 @@ return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
-    		'class' => 'yii\caching\FileCache',
-		],
+            'class' => 'yii\caching\FileCache',
+        ],
     ],
+    'modules' => [
+        'gii' => [
+            'class' => 'yii\gii\Module',
+            // 配置访问IP地址
+            'allowedIPs' => ['127.0.0.1', '::1', '111.204.113.197']
+        ],
+        'debug' => [
+            'class' => 'yii\debug\Module',
+            // 配置访问IP地址
+            'allowedIPs' => ['127.0.0.1', '::1', '111.204.113.197']
+        ],
+    ]
 ];
